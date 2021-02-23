9b310ef2182f1cfe772cd1c0274207d383a3c137
MattheusAlves <matheus_alves0101@outlook.com>
2021-02-23 11:47:55 -0300

    day two

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,16 @@
+import styles from "@/styles/components/ExperienceBar.module.css";
+
+export function ExperienceBar() {
+  return (
+    <header className={styles["experience-bar"]}>
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: "50%" }} />
+        <span className={styles["current-experience"]} style={{ left: "50%" }}>
+          300 xp
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+}
